f093580ae60270d90efda4204b9dddd83ef5b07e
AtharvaChitnis <atharva.chitnis96@gmail.com>
2024-06-15 23:39:43 +0530

    _

--- E-Com/backend/server.js
+++ E-Com/backend/server.js
@@ -2,14 +2,16 @@
 
 //Loading the express library
 const express = require('express');
-
+const path = require('path')
 // Create an express application
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Setting up a route
-app.get('/', (request, result) => {
-  result.send('Hello, World');
-});
+// app.get('/', (request, result) => {
+//   result.send('Hello, World');
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
